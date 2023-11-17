479c9398ca489f54a7f348294cb36af94b162dbc
Christopher Riquelme <criquelmes@live.com>
2023-11-17 03:13:58 -0300

    Sección 27: RESTApi + Websockets - Aplicación de colas

---
+++ src/presentation/services/wss.service.ts
@@ -0,0 +1,41 @@
+import { Server } from "http";
+import { WebSocket, WebSocketServer } from "ws";
+
+interface Options {
+  server: Server;
+  path?: string; // ws
+}
+
+export class WssService {
+  private static _instance: WssService;
+  private wss: WebSocketServer;
+
+  private constructor(options: Options) {
+    const { server, path = "/ws" } = options; // localhost:3000/ws
+
+    this.wss = new WebSocketServer({ server, path });
+    this.start();
+  }
+
+  static get instance(): WssService {
+    if (!WssService._instance) {
+      throw "WssService not initialized";
+    }
+
+    return WssService._instance;
+  }
+  static initWss(options: Options) {
+    if (WssService._instance) {
+      throw "WssService already initialized";
+    }
+    WssService._instance = new WssService(options);
+  }
+
+  public start() {
+    this.wss.on("connection", (ws: WebSocket) => {
+      console.log("Client Connected");
+
+      ws.on("close", () => console.log("Client Disconnected"));
+    });
+  }
+}
